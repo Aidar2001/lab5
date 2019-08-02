18a7c516fbf030d8687e74ecf732ca79709c6299
Alexandr Isaev <ialex@tune-it.ru>
2019-08-02 14:00:37 +0300

    fix: add 'type' for list of interfaces

--- src/main/java/basePackage/objectModel/Human.java
+++ src/main/java/basePackage/objectModel/Human.java
@@ -1,17 +1,13 @@
 package basePackage.objectModel;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Objects;
 
 @Data
 @XmlAccessorType(XmlAccessType.FIELD)
@@ -20,9 +16,9 @@ public class Human extends Creature implements Comparable<Human> {
     private static int number = 1;
     @XmlAttribute
     private int id;
-    @XmlElement(name = "action")
+    @XmlElement(name = "action", type = Action.class)
     private List<IAction> actions = new ArrayList<>();
-    @XmlElement(name = "profession")
+    @XmlElement(name = "profession", type = Profession.class)
     private List<IProfession> professions = new ArrayList<>();
     @XmlElement
     private Location location;
