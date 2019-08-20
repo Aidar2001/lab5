41b4d0d76c1bdf6ba3569ff24907182de23af04e
Aidar <aidarsinetov@gmail.com>
2019-08-20 22:01:46 +0300

    Init new branch

--- src/main/java/basePackage/objectModel/IAction.java
+++ src/main/java/basePackage/objectModel/IAction.java
@@ -4,5 +4,5 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 @JsonDeserialize(as = Action.class)
 public interface IAction {
-    String getActionName();
+    String getActionName(); // lab6
 }
