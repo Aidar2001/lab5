7f571eb4c5c4392fe81d6307a3807b3923a7a2fe
Mikhail Gostev <gostevvm@yandex.ru>
2019-09-18 02:37:32 +0300

    Cleanup project
    Supplement .gitignore
    Remove spare files from project

--- src/main/java/basePackage/objectModel/IProfession.java
+++
@@ -1,8 +0,0 @@
-package basePackage.objectModel;
-
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-
-@JsonDeserialize(as = Profession.class)
-public interface IProfession {
-    String getProfession ();
-}
